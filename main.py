adda2ff736641c25b241dbb2765ace5ec90b21ac
wupasscat <wupasscat10@gmail.com>
2021-03-17 15:53:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 nameList = ["Jason", "Kristian", "Esme", "Keigo", "Sean", "Stella", "Peter", "Spencer", "Leo"]
 
-for item in nameList:
-    print(item)
+def names1():
+    for item in nameList:
+        print(item)
+#names1()
+def names2():
+    for item in nameList:
+        print(item[0])
+#names2()
+def names3():
+    for i in nameList[0]:
+        print(i)
+#names3()
+def names4():
+    print(len(nameList))
+    for i in range(3):
+        #print(i)
+        for name in nameList:
+            print(i, end='')
+        print('')
+            
+names4()
